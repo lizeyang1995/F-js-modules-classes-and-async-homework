1a7788f89c46c1b9a81c778e1a72597363b49d24
lizeyang1995 <424011527@qq.com>
2020-11-11 15:53:28 +0800

    [lizeyang] feat: complete protices

--- src/practice2/Person.js
+++ src/practice2/Person.js
@@ -1,1 +1,20 @@
-export class Person {}
+export default class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  move = () => {
+    console.log(`${this.name} is moving`);
+  };
+}
+
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   move = () => {
+//     console.log(`${this.name} is moving`);
+//   };
+// }
+// export default Person;
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,17 @@
-export const getPoetry
+const url = "https://v1.jinrishici.com/all.json";
+export const getPoetry = () => {
+  const result = fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return [data.origin, data.author, data.content];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+};
